e4fad6a846f4b7e794574a232ef2a31747d5a298
KoalaSat <yv1vtrul@duck.com>
2023-03-07 11:19:51 +0100

    chore: 🔧 Drawer to top

--- src/app/components/MainDrawer/index.tsx
+++ src/app/components/MainDrawer/index.tsx
@@ -5,11 +5,12 @@ import { PixelData } from '../PixelData'
 export const MainDrawer: () => JSX.Element = () => {
   return (
     <Drawer
-      placement='bottom'
+      placement='top'
       closable={false}
       open={true}
       mask={false}
       height={window.innerWidth > 770 ? 135 : 235}
+      width={window.innerWidth}
     >
       <Row gutter={[16, 16]}>
         <Col sm={24} md={16}>
